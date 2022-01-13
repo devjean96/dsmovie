430fa99e4f0c2c41aa0792602e6fc4f0de94e47f
Jean Pereira Moreira - JPR <devjean96@gmail.com>
2022-01-12 23:43:32 -0300

    salvando avaliação

--- backend/src/main/java/com/devsuperior/dsmovie/services/MovieService.java
+++ backend/src/main/java/com/devsuperior/dsmovie/services/MovieService.java
@@ -1,8 +1,5 @@
 package com.devsuperior.dsmovie.services;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
